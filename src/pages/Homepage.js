a7cc1c62f08723ba40c830a8548eb39c89eef789
Nico Gombert <nico.gombert@mercedes-benz.com>
2022-08-16 11:06:34 +0200

    Add Pages directory for Home and Search

---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import Shelf from './components/Shelf'
+import Search from './components/Search'
+
+export default class Homepage extends Component {
+    render() {
+        return (<div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <Shelf/>
+        </div>
+        <Search/>
+      </div>)
+    }
+}
