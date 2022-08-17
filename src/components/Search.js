5985307fc3a71822c95d5b1ff262f4465dfaf57c
Nico Gombert <nico.gombert@mercedes-benz.com>
2022-08-17 11:31:22 +0200

    update search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,68 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
+import "../App.css";
+import {Link} from "react-router-dom";
+import Book from "./Book";
+import {search} from "../BooksAPI";
 
-export default class Search extends Component {
-    render() {
-        return (<div className="open-search">
-        <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-      </div>)
+function Search ({books, setView, inputShelf}) {
+  const [query, setQuery] = useState("")
+  const [result, setResult] = useState([])
+
+  useEffect(() => {
+    const handleQuery = () => {
+      if(query.length>0){
+        search(query.trim()).then(data => {
+          if(data.error) {
+            setResult([])
+            console.log(data.error)
+          }
+          else {
+            data.forEach(d => {
+              let found = false
+              books.forEach(b => {
+                if(d.id === b.id){
+                  d.shelf = b.shelf
+                  found = true
+                }
+              })
+              if(!found) d.shelf = "none"
+            })
+            setResult(data)
+          }
+        }).catch(e => console.log("here",e))
+      }
     }
+    query === "" ? setResult([]) : handleQuery()
+  },[query,books])
+
+  return (
+    <div>
+      {result && <div className = "search-books">
+        <div className = "search-books-bar">
+          <Link to = "/" className = "close-search">
+            Close
+            </Link>
+            <div className = "search-book-input-wrapper">
+              <input
+                type = "text"
+                placeholder = "search by title, author"
+                value = {query}
+                onChange = {(e) => setQuery(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className = "search-books-result">
+            <ol className = "books-grid">
+              {query && result.map(book => (
+                <li key={book.id}>
+                  <Book book={book} inputShelf={inputShelf} setView={setView}/>
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>}
+    </div>
+  )
 }
+
+export default Search
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// https://reactjs.org/docs/strict-mode.html
+
 ReactDOM.render(
 <React.StrictMode>
     <App />
 </React.StrictMode>,
-document.getElementById('root'))
+document.getElementById('root'));
