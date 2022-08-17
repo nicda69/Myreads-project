6861f91a8cb628b2286ec180124a53c20933fb87
Nico Gombert <nico.gombert@mercedes-benz.com>
2022-08-17 13:27:52 +0200

    change book functionality and routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import {Routes, Route} from "react-router-dom";
-// import * as BooksAPI from './BooksAPI'
-import './App.css';
+import {useEffect, useState} from 'react';
+import ".App.css"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Searchpage from './pages/Searchpage';
+import {getAll} from "./BooksAPI";
 
-class BooksApp extends React.Component {
+function BooksApp() {
+  const [books, setBooks] = useState([])
+  const [bookView, setBookView] = useState({})
+  const setAll = () => {
+    getAll().then(all => {
+      if(all) setBooks(all)
+    }).catch(e => console.log(e))
+  }
 
-  render() {
-    return (
+  function updateShelf(shelf, book) {
+    const b = books.filter(b => b.id !== book.id)
+    if(b.length > 0) {
+      const other = books.filter(b => b.id !== book.id)
+      b[0].shelf =shelf
+      setBooks([...other, ...b])
+    } else {
+      book.shelf = shelf
+      setBooks([...books, book])
+    }
+  }
+
+  useEffect(() => {
+    setAll();
+  }, [])
+
+  return (
+    <Router>
       <div className="app">
         <Routes>
           <Route exact path="/" element={<Homepage/>}/>
           <Route exact path="/search" element={<Searchpage/>}/>
         </Routes>
       </div>
-    );
-  }
+    </Router>
+  );
 }
+
 
 export default BooksApp
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,24 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import {update} from "../BooksAPI";
+import {Link} from "react-router-dom";
 
-export default class Book extends Component {
-    render() {
-        return (<li>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">To Kill a Mockingbird</div>
-              <div className="book-authors">Harper Lee</div>
-            </div>
-          </li>)
+
+function Book({book, setView, inputShelf}) {
+  async function updateBook(e){
+    try{
+      await update(book, e.target.value)
+      inputShelf(e.target.value, book)
+    } catch (e) {
+      console.log(e);
     }
+  }
+  return (
+    <div>
+      {(book.author && book.imageLinks) && 
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}>
+            </div>
+              <div className="book-shelf-changer">
+                <select defaultValue = {book.shelf} onChange = {updateBook}>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <Link exact to = "/detail">
+              <div className="book-title" onClick = {() => setView(book)}>
+                {book.title}
+              </div>
+            </Link>
+            {book.author.map(author => (
+              <div key = {book.id + author}>
+                <div className="book-authors">Harper Lee</div>
+              </div>
+            ))}
+        </div>}
+    </div>
+  )
 }
+export default Book
