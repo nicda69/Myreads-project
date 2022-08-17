9c937de5d3ac4bd66c62041afe793b9ce384e94a
Nico Gombert <nico.gombert@mercedes-benz.com>
2022-08-17 11:52:52 +0200

    change component to function in Shelf

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,17 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import Book from "./Book";
+import PropTypes from "prop-types";
 
-export default class Shelf extends Component {
-    render() {
-      return (
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">{this.props.title}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                <Book/>
-              </ol>
-            </div>
-          </div>
-        );
-    }
+function Shelf ({title, books, setView, inputShelf}) {
+  return (
+    <div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map(book => (
+              <li key = {book.id}>
+                <Book book = {book} inputShelf = {inputShelf} setView = {setView}/>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </div>
+  )
 }
